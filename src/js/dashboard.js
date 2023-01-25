7f3303423e540248d3edbcd94e7aa7965075a98f
rinaldiihsannn <rinaldiihsan0401@gmail.com>
2023-01-26 00:51:18 +0700

    pertama

---
+++ src/js/dashboard.js
@@ -0,0 +1,5 @@
+function myFunction() {
+  document.getElementById('dropdownNavbar2').classList.toggle('show');
+  document.getElementById('dropdownNavbar1').classList.toggle('show');
+  document.getElementById('dropdownNavbar0').classList.toggle('show');
+}
